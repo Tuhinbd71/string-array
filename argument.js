cbdb5b5b7777be13d19cce911bcd976ba4068e6e
Tuhinbd71 <m.tuhin02bd@gmail.com>
2022-02-21 00:41:28 +0600

    string commit

---
+++ argument.js
@@ -0,0 +1,7 @@
+function addNumbers(num1, num2) {
+    const result = num1 + num2;
+    return result;
+}
+
+const sum = addNumbers(10, 25);
+console.log(sum);
--- array-sort.js
+++ array-sort.js
@@ -1,3 +1,11 @@
+// array number sort
 const numbers = [4, 1, 6, 7, 2, 9, 3];
 const sortedNumber = numbers.sort();
 console.log(sortedNumber);
+
+// array string sort
+const friends = ['kabir', 'jasim', 'akash', 'tusar', 'fahim', 'basar'];
+const sortName = friends.sort();
+// console.log(sortName);
+const sortReverseName = friends.reverse()
+console.log(sortReverseName);
